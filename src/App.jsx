f1e48ed454d3f70eb1e4c4ddd3ec8a0e28cb288a
Luciano () <luciano1090@gmail.com>
2025-12-30 18:51:15 -0300

    fix: estilos team-logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
-import { useState } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About'
 import './App.css'
 import './pages/Home/Home.css'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
